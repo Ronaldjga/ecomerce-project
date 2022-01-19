803bc568994a9e05ec330f53759a52cb138954f4
Ronaldjga <Ronaldalmeida20022003@gmail.com>
2022-01-19 14:37:19 -0300

    ecomerce complete

--- Public/assets/js/main.js
+++ Public/assets/js/main.js
@@ -15,10 +15,29 @@ const items = [
     },
     {
         id: 2,
-        nome: `Xtudo`,
+        nome: `XTudo`,
         img: `../../frontend/assets/img/xtudo.png`,
         quantidade: 0
+    },
+    {
+        id: 3,
+        nome: `ChesseBurguer`,
+        img: `../../frontend/assets/img/cheeseburguer.png`,
+        quantidade: 0
+    },
+    {
+        id: 4,
+        nome: `ChesseBacon`,
+        img: `../../frontend/assets/img/cheesebacon.png`,
+        quantidade: 0
+    },
+    {
+        id: 5,
+        nome: `XSalada`,
+        img: `../../frontend/assets/img/xsalada.png`,
+        quantidade: 0
     }
+
 ];
 
 inicializaLoja = () => {
@@ -26,10 +45,10 @@ inicializaLoja = () => {
     items.map((valor) => {
         containerProdutos.innerHTML += `
         
-        <div class="produtos-single">
+        <div class="produtos-single flex flex-col items-center justify-center bg-laranjaPaleta-Primary w-96 h-96 border-4 border-amareloPaleta-900">
             <img class="w-32 h-32" src="${valor.img}"/>
-            <p>${valor.nome}</p>
-            <a class="links" key="${valor.id}" href="#">Adicionar ao carrinho</a>
+            <p class="text-3xl text-center font-bold">${valor.nome}</p>
+            <button class="links" key="${valor.id}">Adicionar ao carrinho</button>
         </div>
         
         `
@@ -38,10 +57,19 @@ inicializaLoja = () => {
 inicializaLoja();
 
 atualizaCarrinho = () => {
-    console.log(items);
+    var containerCarrinho = document.getElementById('carrinho');
+    containerCarrinho.innerHTML = '';
+    items.map((valor) => {
+        if (valor.quantidade > 0) {
+            containerCarrinho.innerHTML += `
+            <p class="my-3"><span class="text-2xl font-bold">${valor.nome}</span> | quantidade:${valor.quantidade}</p>
+            <hr>
+            `;
+        }
+    })
 };
 
-var links = document.getElementsByTagName('a')
+var links = document.getElementsByTagName('button')
 
 for (var i = 0; i < links.length; i++){
     links[i].addEventListener('click',function(){
